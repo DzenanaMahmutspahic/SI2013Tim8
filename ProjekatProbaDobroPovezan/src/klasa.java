71e4b03bd4183d41658773483e65e851d059bf8b
TajmaKovacevic <tkovacevic1@etf.unsa.ba>
2014-05-04 13:43:03 +0200

    ProjekatProba

---
+++ ProjekatProbaDobroPovezan/src/klasa.java
@@ -0,0 +1,11 @@
+
+public class klasa {
+	public static void main(String[] arg)
+	{
+		System.out.println("Tajma");
+		System.out.println("Tajma");
+		System.out.println("Tajma");
+		System.out.println("Tajma");
+		System.out.println("Tajma");
+	}
+}
