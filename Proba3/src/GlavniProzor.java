567166aec1091420348c8a56d6b119dd60012fb1
TajmaKovacevic <tkovacevic1@etf.unsa.ba>
2014-05-04 15:19:35 +0200

    4.5.

---
+++ Proba3/src/GlavniProzor.java
@@ -0,0 +1,52 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+
+
+public class GlavniProzor {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GlavniProzor window = new GlavniProzor();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GlavniProzor() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnNewButton = new JButton("New button");
+		frame.getContentPane().add(btnNewButton);
+	}
+
+}
--- Proba3/src/Tajma.java
+++ Proba3/src/Tajma.java
@@ -1,15 +1,17 @@
+import javax.swing.JFrame; 
+import javax.swing.JLabel;
 
-public class Tajma {
+public class Tajma extends JFrame{
 
 	public static void main(String[] arg) {
-		System.out.println("Hello world!");
-		// final koristimo kao const, vrijednost koja se ne moze miejnjati
-		// inner class-da bi se pristupili necemo u inner mora biti final
-		System.out.println("Hello world!");
-		System.out.println("Hello world!");
-		System.out.println("Hello world!");
-		System.out.println("Hello world!");
-//		tajma
+		new Tajma();
+
 		
 	}
+	Tajma() { 
+		 JLabel helloLabel = new JLabel("Hello World"); 
+		 add(helloLabel); 
+		 this.setSize(100, 100); 
+		 setVisible(true); 
+		 }
 }
