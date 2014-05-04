55dbdeff2f9cf45029a6c9719c4189e511da0ad4
TajmaKovacevic <tkovacevic1@etf.unsa.ba>
2014-05-04 14:09:16 +0200

    proba3projekat

---
+++ Proba3/src/Tajma.java
@@ -0,0 +1,15 @@
+
+public class Tajma {
+
+	public static void main(String[] arg) {
+		System.out.println("Hello world!");
+		// final koristimo kao const, vrijednost koja se ne moze miejnjati
+		// inner class-da bi se pristupili necemo u inner mora biti final
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+//		tajma
+		
+	}
+}
