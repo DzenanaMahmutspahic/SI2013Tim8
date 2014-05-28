0590fa257f244ced2849eba8d59d4d6bffe4a256
TajmaKovacevic <tkovacevic1@etf.unsa.ba>
2014-05-28 23:54:26 +0200

    dodavanje sobe

--- Hotel/src/main/java/etf/si/tim8/app/Hotel/EkranZaDodavanjeSobe.java
+++ Hotel/src/main/java/etf/si/tim8/app/Hotel/EkranZaDodavanjeSobe.java
@@ -5,15 +5,28 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.BevelBorder;
+
 import java.awt.Color;
 import java.awt.SystemColor;
+
 import javax.swing.JLabel;
 import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
 import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.JButton;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import Hibernate.HibernateUtil;
+import Klase.Soba;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
 public class EkranZaDodavanjeSobe {
 //tajma komentar
@@ -89,20 +102,21 @@ public class EkranZaDodavanjeSobe {
 		lblCijena.setBounds(10, 86, 66, 14);
 		panel_1.add(lblCijena);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Balkon");
-		chckbxNewCheckBox.setBackground(Color.WHITE);
-		chckbxNewCheckBox.setBounds(10, 112, 102, 18);
-		panel_1.add(chckbxNewCheckBox);
+		final JCheckBox cb1 = new JCheckBox("Balkon");
+		cb1.setBackground(Color.WHITE);
+		cb1.setBounds(10, 129, 102, 18);
+		panel_1.add(cb1);
 		
-		JSpinner spinner = new JSpinner();
+		final JSpinner spinner = new JSpinner();
 		spinner.setBounds(65, 8, 86, 20);
 		panel_1.add(spinner);
 		
-		JSpinner spinner_1 = new JSpinner();
+		final JSpinner spinner_1 = new JSpinner();
 		spinner_1.setBounds(65, 36, 86, 20);
 		panel_1.add(spinner_1);
 		
-		JComboBox comboBox = new JComboBox();
+		final JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Jednokrevetna", "Dvokrevetna"}));
 		comboBox.setBounds(65, 61, 86, 20);
 		panel_1.add(comboBox);
 		
@@ -157,7 +171,44 @@ public class EkranZaDodavanjeSobe {
 		panel_2.add(rdbtnTrezor);
 		
 		JButton btnDodajSobu = new JButton("Dodaj sobu");
+		
+	
+		btnDodajSobu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//private long id;
+				//private int brojSobe;
+				//private int sprat;
+				//private Boolean balkon;
+				//private int brojKreveta;
+				//private double cijena;
+				//private Boolean zauzeta;
+				Soba soba = new Soba();
+				Session session = HibernateUtil.getSessionFactory().openSession();
+				Transaction t = session.beginTransaction();
+				soba.setBrojSobe(Integer.parseInt(spinner_1.getValue().toString()));
+				soba.setSprat(Integer.parseInt(spinner.getValue().toString()));
+				if(cb1.isSelected())soba.setBalkon(true);
+				else soba.setBalkon(false);
+				if(comboBox.getSelectedIndex()==0)
+					soba.setBrojKreveta(1);
+				else
+					soba.setBrojKreveta(2);
+				try{
+					soba.setCijena(Double.parseDouble(textField.getText()));
+					}
+				catch(Exception e){
+						JOptionPane.showMessageDialog(null, "Morate unijeti broj!", "Greska", JOptionPane.INFORMATION_MESSAGE);
+					} 
+					
+				soba.setZauzeta(false);
+				session.save(soba);
+				t.commit();
+			}
+		});
+		
 		btnDodajSobu.setBounds(294, 236, 102, 23);
 		panel_1.add(btnDodajSobu);
+		
 	}
+
 }
