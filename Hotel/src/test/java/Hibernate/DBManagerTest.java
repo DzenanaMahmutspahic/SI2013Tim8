b56c2e30e310b8be86e87808a1571afe353d2ffa
Dzenana Mahmutspahic <dmahmutspa1@etf.unsa.ba>
2014-05-30 14:53:20 +0200

    Testna klasa DBManagerTest

---
+++ Hotel/src/test/java/Hibernate/DBManagerTest.java
@@ -0,0 +1,43 @@
+package Hibernate;
+
+import junit.framework.TestCase;
+
+public class DBManagerTest extends TestCase {
+
+	public void testProvjeriLogin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testDajBoravke() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testDajBoravke2() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testDajZauzeteSobe() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testDajPredracun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testDajRacun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testUnesiPredracun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testUpdatePredracun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testUnesiRacun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
