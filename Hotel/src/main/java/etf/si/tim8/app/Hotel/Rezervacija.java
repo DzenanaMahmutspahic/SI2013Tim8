1b733896dab54e1e122b5545373484244296aadc
dinokraljusic <kraljushic@gmail.com>
2014-06-09 22:54:50 +0200

    Ispravljen Issue #84

--- Hotel/src/main/java/etf/si/tim8/app/Hotel/Rezervacija.java
+++ Hotel/src/main/java/etf/si/tim8/app/Hotel/Rezervacija.java
@@ -178,6 +178,7 @@ public class Rezervacija extends javax.swing.JFrame {
         jTextField16 = new javax.swing.JTextField();
         jLabel27 = new javax.swing.JLabel();
         jComboBox2 = new javax.swing.JComboBox();
+        jComboBox2.setEnabled(false);
         jButton5 = new javax.swing.JButton();
         jButton5.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
